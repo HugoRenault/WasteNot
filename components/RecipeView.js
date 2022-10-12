febbaedd1f021dcd825124c7727087201ff5719f
HugoRenault <hugorenault2304@gmail.com>
2022-10-12 09:50:59 +0200

    changements louis

--- components/RecipeView.js
+++ components/RecipeView.js
@@ -15,35 +15,44 @@ var ranking = 2 // minimize missing ingredients first
 var number = 1
 ///////////////////////
 
-function buildUrl(params){
-  return(
-    params.url + '&ingredients=' + params.ingredients + '&ignorePantry=' + params.ignorePantry + '&ranking=' + params.ranking + '&number=' + params.number
-  )
-}
-
-const Recipes = async(flag) => {
-  let params = {
-    'url' : API_URL_FIND_BY_INGREDIENTS,
-    'ingredients' : ingredients_example,
-    'ignorePantry' : ignorePantry,
-    'ranking' : ranking,
-    'number' : number
-  }
-  var recipesList = []
-  recipesList = await fetch(buildUrl(params)).then((data) => {
-    flag(true);
-    return(data.json())
-  });
-  return(recipesList)
-}
 
 export default function  RecipeView () {
-    const [loaded, setIsLoaded] = useState(false)
-    objs = Recipes(setIsLoaded)
-    if(loaded){
-       return(objs.map((obj) => {return(<Text>{obj.title}</Text>)}));
-     }
-     return(<Text>Loading...</Text>);
-}
 
+  const [loaded, setIsLoaded] = useState(false)
+
+
+  function buildUrl(params){
+    return(
+      params.url + '&ingredients=' + params.ingredients + '&ignorePantry=' + params.ignorePantry + '&ranking=' + params.ranking + '&number=' + params.number
+    )
+  }
+  
+  const Recipes = async(flag) => {
+    let params = {
+      'url' : API_URL_FIND_BY_INGREDIENTS,
+      'ingredients' : ingredients_example,
+      'ignorePantry' : ignorePantry,
+      'ranking' : ranking,
+      'number' : number
+    }
+    var recipesList = []
+    recipesList = await fetch(buildUrl(params)).then((data) => {
+      setIsLoaded(true);
+      return(data.json())
+    });
+    return(recipesList)
+  }
+  
+  objs = Recipes().then((data)=>{
+    return(data.map((obj) => {
+      return(<Text>{obj.title}</Text>)
+    }))
+  })
+  if(loaded){
+      <Text>aaazz</Text>
+  }
+  else {
+    return(<Text>Loading...</Text>);
+  }
+}
 const styles = StyleSheet.create({})
